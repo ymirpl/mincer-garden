63de5664629fcaa4d5a0740ec903679eb04db8d4
Marcin Mincer <marcin.mincer@gmail.com>
2026-05-03 23:12:00 +0200

    fix: add base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://garden.mincer.capital',
+  site: 'https://ymirpl.github.io',
+  base: '/mincer-garden',
   trailingSlash: 'never',
   integrations: [sitemap()],
   markdown: {
